f1f5ea84012a13a9d8655d28c668a285cabedceb
abbb03 <alecksey17@icloud.com>
2022-11-05 16:45:09 +0300

    fix mongodb uri

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost/gocloud')],
+    imports: [MongooseModule.forRoot(process.env.DB_URI || 'mongodb://localhost/gocloud')],
 })
 export class DatabaseModule {}
